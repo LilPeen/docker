81e50f92fb2936d82f22e51a8d6630dcbdea780d
Emir Orsini <deranged.ve@gmail.com>
2023-05-03 02:00:11 +0000

    Fixed maybe

---
+++ docker-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Step 1: Bring down the docker containers and remove volumes
+docker compose down --volumes
+
+# Step 2: Remove the image with the specified name
+yes | docker image prune -a
+
+# Step 3: Start the containers in detached mode
+docker compose up -d
+
+# Finished
+echo "Docker setup completed."
